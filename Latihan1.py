5977d34c3a6bf2b8c1d142b8ed2bc4e033561b68
Linda Wahyu Setyoningsih <56390857+LindaWahyuSetyoningsih@users.noreply.github.com>
2019-10-26 12:46:07 +0700

    Add files via upload

---
+++ Latihan1.py
@@ -0,0 +1,9 @@
+namalengkap = "Linda Wahyu Setyoningsih"
+namapanggilan = "Linda"
+npm = "311910557"
+tempatlahir = "Bekasi"
+umur = "18"
+telf = "0895326388827"
+alamat = "Cibitung"
+
+print("Assalamualaikum, \n Let me introduce my self, My name is",namalengkap,", but you can call me",namapanggilan,", My npm is",npm,", I was born",tempatlahir,"and I am",umur,"years old. I am very glad if you want to invite my house in",alamat,", So, don't forget to call me before with the number",telf,"\n Thank You")
